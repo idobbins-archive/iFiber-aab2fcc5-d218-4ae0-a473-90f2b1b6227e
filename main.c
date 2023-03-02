e57d14b5ada62323dc6a6eae46d28d4769cdf62e
idobbins <isaac.dobbins@icloud.com>
2023-03-01 19:31:47 -0700

    initial fiber execution test

--- main.c
+++ main.c
@@ -2,19 +2,33 @@
 #include <context.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+
+int fib(int n) {
+  if (n <= 1)
+    return n;
+  return fib(n - 1) + fib(n - 2);
+}
+
+void fiber() {
+  printf("fib(32) = %d", fib(32));
+  exit (0);
+}
 
 int main() {
+
+  char data[1024];
+
+  char *sp = (char *)(data + sizeof data);
+  sp = (char *)((uintptr_t)sp & -16L);
+  sp -= 128;
+
   volatile int a = 0;
 
   struct icontext c;
 
-  icontext_get(&c);
+  c.r_return = (void*)fiber;
+  c.r_stack = sp;
 
-  printf("hello world\n");
-
-  if (a == 0) {
-    a = 1;
-    icontext_set(&c);
-  }
-
+  icontext_set(&c);
 }
